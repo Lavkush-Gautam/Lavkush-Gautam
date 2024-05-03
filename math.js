b468c673a0a7315d2559e81b40bb59ddcf38c708
Lavkush-Gautam <154265093+Lavkush-Gautam@users.noreply.github.com>
2024-05-03 09:30:33 -0700

    Add files via upload

---
+++ math.js
@@ -0,0 +1,13 @@
+// export const sum=(a,b)=> a+b;//or module.export=sum=(a,b)=> a+b;
+// export const mul=(a,b)=> a*b;
+// export const g=9.8;
+// export const pi=3.14;
+
+// //module.exports=123;
+// module.exports={
+//     sum:sum,
+//     mul:mul,
+//     g:9.8,
+//     pi:3.14,
+// }
+
